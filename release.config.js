c264be46d3b4c32403f147a0cc8fdb651823131a
fwx5618177 <fengwenxuan2006@126.com>
2024-05-17 02:06:31 +0800

    feat: add semantic release and release assets

--- release.config.js
+++ release.config.js
@@ -1,15 +1,42 @@
 export default {
-    branches: ['main'],
+    tagFormat: 'v${version}',
+    branches: [
+        'master',
+        'next',
+        {
+            name: 'beta',
+            prerelease: true,
+        },
+        {
+            name: 'alpha',
+            prerelease: true,
+        },
+    ],
     plugins: [
         '@semantic-release/commit-analyzer', // 分析提交信息来决定版本号
         '@semantic-release/release-notes-generator', // 生成发布说明
-        '@semantic-release/changelog', // 更新 CHANGELOG.md 文件
+        [
+            '@semantic-release/changelog',
+            {
+                'changelogFile': 'CHANGELOG.md',
+            },
+        ],
         '@semantic-release/npm', // 更新版本号并发布到 npm
         [
             '@semantic-release/git',
             {
                 assets: ['package.json', 'CHANGELOG.md'],
                 message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+            },
+        ],
+        [
+            '@semantic-release/github',
+            {
+                'assets': [
+                    {
+                        'path': './build/assets.tar.gz',
+                    },
+                ],
             },
         ],
     ],
